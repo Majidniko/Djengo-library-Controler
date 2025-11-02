e9479a65237f9789ffd6e8ec01831e11e217873b
Majid Nikoubin <110221028+Majidniko@users.noreply.github.com>
2025-11-02 13:49:11 +0330

    ایجاد بخش احراز هویت
    در این بخش فرآیند احراز هویت با استفاده از توکن ایجاد شده است
    با استفاده از مستندات موجود rest_framework.authtoken پیاده سازی گزدید تا با استفاده از آدرس register بتوتن کاربری را ثبت نام نمود و با استفاده از login کاربر بتواند به سیستم وارد شده و در نزم افزار postman  توکن تولیدی را ببیند.در این میان در بخش urls آدرس دو View ساخته شده برای فراهم کردن این دو موضوع قرار داده شد.
    با توجه به مشکلات زبان فارسی در فایل txt جهت بهتر قرار دادن فرآیند حل مسله ، فالی راهنمای دانش اضافه گردید تا تمام مراحل در آن جهت استفادده های بعدی قرارگیرد، در هر مرحله دانش کسب شده با استفاده از لینک محتوای جستجو شده قرار خواهد  گرفت.

---
+++ users/views.py
@@ -0,0 +1,33 @@
+from django.shortcuts import render
+
+# Create your views here.
+from rest_framework.permissions import AllowAny
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from django.contrib.auth.models import User
+from rest_framework.authtoken.models import Token
+from django.contrib.auth import authenticate
+
+class RegisterView(APIView):
+    permission_classes = [AllowAny]
+    def post(self, request):
+        username = request.data.get("username")
+        password = request.data.get("password")
+        if User.objects.filter(username=username).exists():
+            return Response({"error": "User already exists"}, status=400)
+        user = User.objects.create_user(username=username, password=password)
+        token, _ = Token.objects.get_or_create(user=user)
+        return Response({"token": token.key}, status=201)
+
+class LoginView(APIView):
+    permission_classes = [AllowAny]
+    # "token": "ebb49f6d0d32a31ae8f8226527049a6dd3b709fb"
+    def post(self, request):
+        username = request.data.get("username")
+        password = request.data.get("password")
+        user = authenticate(username=username, password=password)
+        if not user:
+            return Response({"error": "Invalid credentials"}, status=400)
+        token, _ = Token.objects.get_or_create(user=user)
+        return Response({"token": token.key})
