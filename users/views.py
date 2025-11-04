8d32fc1cfb68842c9664fd965d0e883c3b9c2a42
Majid Nikoubin <110221028+Majidniko@users.noreply.github.com>
2025-11-04 18:15:46 +0330

    اصلاحات 1
    اصلاح برای درج فیل ها در Api که در حالتی عادی swegger آنها را نمیشناخت و در تغریف view آن ها را به آن معرفی نمودم

--- users/views.py
+++ users/views.py
@@ -8,9 +8,19 @@ from rest_framework import status
 from django.contrib.auth.models import User
 from rest_framework.authtoken.models import Token
 from django.contrib.auth import authenticate
+from drf_yasg.utils import swagger_auto_schema
+from rest_framework import serializers
 
+class RegisterSerializer(serializers.Serializer):
+    username = serializers.CharField(max_length=150)
+    password = serializers.CharField(write_only=True)
+    
 class RegisterView(APIView):
-    permission_classes = [AllowAny]
+    @swagger_auto_schema(
+    request_body=RegisterSerializer,
+    responses={201: "User created successfully", 400: "Bad Request"}
+    )
+    # permission_classes = [AllowAny]
     def post(self, request):
         username = request.data.get("username")
         password = request.data.get("password")
@@ -20,8 +30,17 @@ class RegisterView(APIView):
         token, _ = Token.objects.get_or_create(user=user)
         return Response({"token": token.key}, status=201)
 
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField(write_only=True)
+
+
 class LoginView(APIView):
-    permission_classes = [AllowAny]
+    @swagger_auto_schema(
+        request_body=LoginSerializer,
+        responses={200: "Token returned", 400: "Invalid credentials"}
+    )
+    # permission_classes = [AllowAny]
     # "token": "ebb49f6d0d32a31ae8f8226527049a6dd3b709fb"
     def post(self, request):
         username = request.data.get("username")
@@ -30,4 +49,4 @@ class LoginView(APIView):
         if not user:
             return Response({"error": "Invalid credentials"}, status=400)
         token, _ = Token.objects.get_or_create(user=user)
-        return Response({"token": token.key})
+        return Response({"token": token.key}, status=status.HTTP_200_OK)
